8344181effdc09745900e8b77f6e71f29f7f1f32
nikhilpingale06 <nikhilpingale06@gmail.com>
2025-03-04 11:44:26 +0530

    File structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical chatbot Project',
+    version= '0.0.0',
+    author= 'Nikhil Pingale',
+    author_email= 'nikhilpingale06@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
